483d9b7810fc6b3b71b9ce6312c418f583014833
Akshay <mohite.akshay118@gmail.com>
2018-03-29 13:23:32 +0530

    added edit for users

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     req.getConnection(function(error, conn) {
         conn.query('SELECT * FROM users ORDER BY id DESC',function(err, rows, fields) {
-            res.render('index', { title: 'User Listing',result:rows });
+            console.log(rows);
+            res.render('users', { title: 'User Listing',results:rows });
         });
     });
 
@@ -15,4 +16,31 @@ router.get('/add', function(req, res, next) {
   res.render('add', { title: 'Add User' });
 });
 
+router.get('/edit/(:id)', function(req, res, next) {
+    req.getConnection(function(error, conn) {
+        conn.query('SELECT * FROM users WHERE id = ' + req.params.id,function(err, rows, fields) {
+            res.render('edit', { title: 'Edit User',results:rows });
+        });
+    });
+});
+
+router.post('/edit/(:id)', function(req, res, next) {
+    var user = {
+        name: req.params.name.escape().trim(),
+        age: req.params.age.escape().trim(),
+        email: req.params.email.escape().trim(),
+        password: req.params.password.escape().trim(),
+
+    }
+    
+    req.getConnection(function(error, conn) {
+            conn.query('UPDATE users SET ? WHERE id = ' + req.params.id, user, function(err, result) {
+                if (err) {
+                    res.redirect('/users')
+
+                }
+        });
+    });
+                
+});
 module.exports = router;
